c6fe41e00b557dbdfb7214f9bbfc32627c510490
Cho-Hyun-Seung <toki0327@naver.com>
2025-08-18 00:08:17 +0900

    feat: query req, res 추가

---
+++ src/main/java/com/nz/nomadzip/copythis/query/dto/request/Request.java
@@ -0,0 +1,9 @@
+package com.nz.nomadzip.copythis.query.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class Request {
+}
---
+++ src/main/java/com/nz/nomadzip/copythis/query/dto/response/Response.java
@@ -0,0 +1,7 @@
+package com.nz.nomadzip.copythis.query.dto.response;
+
+import lombok.Builder;
+
+@Builder
+public class Response {
+}
