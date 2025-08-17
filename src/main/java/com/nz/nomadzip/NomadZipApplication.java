23a990b250317e03a4386e3a7578b82970ba16e9
Cho-Hyun-Seung <toki0327@naver.com>
2025-08-17 23:29:54 +0900

    chore: 초기 설정

---
+++ src/main/java/com/nz/nomadzip/NomadZipApplication.java
@@ -0,0 +1,13 @@
+package com.nz.nomadzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadZipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NomadZipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nz/nomadzip/NomadZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nz.nomadzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadZipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
