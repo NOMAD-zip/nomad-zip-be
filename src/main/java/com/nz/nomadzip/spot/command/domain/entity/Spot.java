4b4172045ec94eb3e280fc3f3465885ac78148a9
Cho-Hyun-Seung <toki0327@naver.com>
2025-10-16 02:08:52 +0900

    fix: 관계형 테이블 생성을 위한 엔티티 수정

--- src/main/java/com/nz/nomadzip/spot/command/domain/entity/Spot.java
+++ src/main/java/com/nz/nomadzip/spot/command/domain/entity/Spot.java
@@ -1,4 +1,12 @@
 package com.nz.nomadzip.spot.command.domain.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "spot")
 public class Spot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long spotId;
 }
--- src/main/java/com/nz/nomadzip/user/command/domain/entity/User.java
+++ src/main/java/com/nz/nomadzip/user/command/domain/entity/User.java
@@ -1,4 +1,17 @@
 package com.nz.nomadzip.user.command.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
 }
